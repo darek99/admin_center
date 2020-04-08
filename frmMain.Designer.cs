c7babb0857c64fff54b905b28e1ba865142bb8e6
Dariusz Gmyz <darek@vertimo.pl>
2020-04-08 09:45:53 +0200

    Dodałem ikonę.

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.msconfigButton = new System.Windows.Forms.Button();
             this.appButton = new System.Windows.Forms.Button();
@@ -187,6 +188,7 @@
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
