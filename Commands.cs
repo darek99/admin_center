b75d21309c374d0567728b6c2e1c4f9f6e0b8981
Dariusz Gmyz <darek@vertimo.pl>
2021-11-25 18:11:18 +0100

    added new command allowed you to delete backups keeping x last versions

--- Commands.cs
+++ Commands.cs
@@ -8,6 +8,7 @@
         public readonly string MSCONFIG = "msconfig.exe";
         public readonly string TASKSCHD = "taskschd.msc";
         public readonly string WBADMIN = "wbadmin.msc";
+        public readonly string WBADMIN_DELETE = "wbadmin delete backup -keepversions:{0}";
 
         public readonly string UPDATE = "https://drive.google.com/drive/folders/1-RFHxjVz8aULLFbp4CYP1VcgUGM6Pc0p?usp=sharing";
         public readonly string UPDATE2 = "https://app.box.com/s/oxfhptwr9lqx6nop5y1e852etkr1z3pg";
